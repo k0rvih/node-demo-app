d75ad2dbc812d814c9469e43aa3e647763a664c6
Oleksandr Barbul <Oleksandr_Barbul@epam.com>
2020-01-05 19:18:24 +0200

    revert Update02

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('!Hello Epam!');
+  res.send('Hello Epam!');
 });
 
 var server = app.listen(3000, function () {
